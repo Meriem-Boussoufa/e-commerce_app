790f83c7415582c35bd2d591ef4dbd4d8282f3ec
meriem <meriem@gmail.com>
2024-02-23 19:45:58 +0100

    organise code and implement singin method

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:e_commerce_app/pages/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,6 +14,35 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences? preferences;
+  bool loading = false;
+  bool isLoggedin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    setState(() {
+      loading = true;
+    });
+
+    preferences = await SharedPreferences.getInstance();
+    isLoggedin = await googleSignIn.isSignedIn();
+
+    if (isLoggedin == true) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
