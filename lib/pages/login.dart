403451e0469ee3de64e5921fa0b4d9aea14c6331
meriem <meriem@gmail.com>
2024-02-23 20:23:22 +0100

    Handle Sign in

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce_app/pages/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -41,6 +43,49 @@ class _LoginState extends State<Login> {
     setState(() {
       loading = false;
     });
+  }
+
+  Future handleSignIn() async {
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      loading = true;
+    });
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleUser!.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    UserCredential userCredential =
+        await firebaseAuth.signInWithCredential(credential);
+    User? user = userCredential.user;
+    if (user != null) {
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection("users")
+          .where("id", isEqualTo: user.uid)
+          .get();
+      final List<DocumentSnapshot> documents = result.docs;
+      if (documents.isEmpty) {
+        FirebaseFirestore.instance.collection("users").doc(user.uid).set({
+          "id": user.uid,
+          "username": user.displayName,
+          "profilePicture": user.photoURL,
+        });
+        await preferences!.setString("id", user.uid);
+        await preferences!.setString("username", user.displayName!);
+        await preferences!.setString("profilePicture", user.photoURL!);
+      } else {
+        await preferences!.setString("id", documents[0]["id"]);
+        await preferences!.setString("username", documents[0]["username"]);
+        await preferences!
+            .setString("profilePicture", documents[0]["profilePicture"]);
+      }
+      Fluttertoast.showToast(msg: "Login was successful");
+      setState(() {
+        loading = false;
+      });
+    } else {}
   }
 
   @override
